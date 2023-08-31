52861f863e12aa08198cf1cc47a28145b4398976
Paul Claret <p4ul.claret@gmail.com>
2023-08-31 11:36:14 +0200

    changing routes name and adding bcrypt to project

--- backend/go.mod
+++ backend/go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jmoiron/sqlx v1.3.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -24,20 +24,27 @@ func main() {
     userRouter := r.PathPrefix("/users").Subrouter();
 
     authRouter := userRouter.PathPrefix("/auth").Subrouter();
+    authRouter.HandleFunc("/login", users.GetLogin).Methods("GET");
     authRouter.HandleFunc("/login", users.Login).Methods("POST");
-    authRouter.HandleFunc("/logout", users.Logout).Methods("GET");
+    authRouter.HandleFunc("/register", users.GetRegister).Methods("GET");
+    authRouter.HandleFunc("/register", users.Register).Methods("POST");
+    authRouter.HandleFunc("/logout", users.Logout).Methods("POST");
     
-    profileRouter := userRouter.PathPrefix("/profile").Subrouter();
-    profileRouter.Use(authenticationMiddleware); // for every requests made to /users/profile/*, the middel ware will do its job
-    profileRouter.HandleFunc("/{uid:[0-9]+}", users.GetProfile).Methods("GET");
-    profileRouter.HandleFunc("/{uid:[0-9]+}", users.CreateProfile).Methods("POST");
+    accountRouter := userRouter.PathPrefix("").Subrouter();
+    accountRouter.Use(authenticationMiddleware); // for every requests made to /users/profile/*, the middel ware will do its job
+    accountRouter.HandleFunc("/profile", users.GetProfile).Methods("GET");  // get profile info route
+    accountRouter.HandleFunc("/profile", users.PostProfile).Methods("POST"); // update profile route
+    accountRouter.HandleFunc("/settings", users.GetSettings).Methods("GET"); // get current settings route
+    accountRouter.HandleFunc("/settings", users.PostSettings).Methods("POST");// change settings route
+    // profileRouter.HandleFunc("/{uid:[0-9]+}", users.GetProfile).Methods("GET");
+    // profileRouter.HandleFunc("/{uid:[0-9]+}", users.CreateProfile).Methods("POST");
 
 
-    bibleactionRouter := r.PathPrefix("/bible").Subrouter()
-    bibleactionRouter.HandleFunc("/getverses/{edition:[0-9]+}/{book:[0-9]+}/{chapter:[0-9]+}/{versefrom:[0-9]+}/{verseto:[0-9]+}", bible.GetVerses).Methods("GET");
-    bibleactionRouter.HandleFunc("/geteditions", bible.GetBibleEditions)
-    bibleactionRouter.HandleFunc("/getbooksofedition/{edition:[0-9]+}", bible.GetBibleBooksFromEdition)
-    bibleactionRouter.HandleFunc("/getnumberofversefrombookfromedition/{edition:[0-9]+}/{book:[0-9]+}", bible.GetNumberOfVersesFromBookFromEdition)
+    bibleRouter := r.PathPrefix("/bible").Subrouter()
+    bibleRouter.HandleFunc("/getverses/{edition:[0-9]+}/{book:[0-9]+}/{chapter:[0-9]+}/{versefrom:[0-9]+}/{verseto:[0-9]+}", bible.GetVerses).Methods("GET");
+    bibleRouter.HandleFunc("/geteditions", bible.GetBibleEditions)
+    bibleRouter.HandleFunc("/getbooksofedition/{edition:[0-9]+}", bible.GetBibleBooksFromEdition)
+    bibleRouter.HandleFunc("/getnumberofversefrombookfromedition/{edition:[0-9]+}/{book:[0-9]+}", bible.GetNumberOfVersesFromBookFromEdition)
 
     srv := &http.Server{
         Handler:      r,
@@ -64,6 +71,27 @@ func main() {
 
 ===================================
 */
+
+func requireDatabaseMiddleware(next http.Handler) http.Handler {
+    // if the database connection doesn't exists, create it
+    if services.DB == nil {
+        fmt.Println("database is nil, creating it")
+        services.Init_DB() // this will try 5 times to connect, spaced in time by 5 seconds
+    }
+    // check if connection was successful (if not return error else call next)
+    if services.DB == nil {
+        fmt.Println("Couldn't connect to database exiting")
+        return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+            w.WriteHeader(http.StatusInternalServerError)
+            w.Write([]byte("Server side error with the database connection sry"))
+            // return
+        })
+    } else {
+        return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+            next.ServeHTTP(w, r)
+        })
+    }
+}
 
 func authenticationMiddleware(next http.Handler) http.Handler {
     // if the database connection doesn't exists, create it
@@ -94,7 +122,7 @@ func authenticationMiddleware(next http.Handler) http.Handler {
         fmt.Println(token)
         var storage  struct {
             Sid string `db:"sid"`
-            Value string `db:"value"`
+            Value string `db:"value"` // base64 encoded json data
         }
         err = services.DB.Get(&storage, "SELECT * FROM Sessions WHERE sid=$1", token.Value) // ignore the lsp error
         if err != nil {
